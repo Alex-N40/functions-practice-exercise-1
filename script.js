98eb5163f16c345122c40b2e7ddf9c49709c1e50
Alex-N40 <alex_n40@proton.me>
2024-04-30 09:58:16 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var shoutIt = function (name) {
+  var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+
+console.log(shoutIt("Alex"));
+console.log(shoutIt("Kim"));
+console.log(shoutIt("Joe"));
+console.log(shoutIt("Sam"));
